010ffb488ec7ef0b52dbd5f09b2e3bba19021fb1
BOBKOSKIE <rb868x@att.com>
2022-05-29 17:45:48 -0400

    Updates

---
+++ precache-manifest.bcfd6d07b16a1f7d81c2a43364c54710.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c2b7fcbc454a5a951c93",
+    "url": "/react-att/static/css/main.edb6831c.chunk.css"
+  },
+  {
+    "revision": "c2b7fcbc454a5a951c93",
+    "url": "/react-att/static/js/main.c2b7fcbc.chunk.js"
+  },
+  {
+    "revision": "0b5adef5a8e99730695f",
+    "url": "/react-att/static/js/1.0b5adef5.chunk.js"
+  },
+  {
+    "revision": "1c211ffe08a8097aeac6",
+    "url": "/react-att/static/js/2.1c211ffe.chunk.js"
+  },
+  {
+    "revision": "fe571902547b929f7bb7",
+    "url": "/react-att/static/js/runtime~main.fe571902.js"
+  },
+  {
+    "revision": "b8293f906f4320d091f063507203fbae",
+    "url": "/react-att/index.html"
+  }
+];
