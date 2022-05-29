6c5b8ab708612d1844bbc8745df12f4d260c5128
BOBKOSKIE <rb868x@att.com>
2022-05-29 18:19:14 -0400

    add images

--- src/components/DragDrop.js
+++ src/components/DragDrop.js
@@ -2,22 +2,25 @@ import React, { useState } from "react";
 import Picture from "./Picture";
 import { useDrop } from "react-dnd";
 import "../App.css";
-
+console.log("UPDATE");
 const PictureList = [
   {
     id: 1,
     url:
-      "https://yt3.ggpht.com/ytc/AAUvwnjOQiXUsXYMs8lwrd4litEEqXry1-atqJavJJ09=s900-c-k-c0x00ffffff-no-rj",
+      // "https://yt3.ggpht.com/ytc/AAUvwnjOQiXUsXYMs8lwrd4litEEqXry1-atqJavJJ09=s900-c-k-c0x00ffffff-no-rj",
+      "https://rbobkoskie1.github.io/react-att/images/Family_1.jpg",
   },
   {
     id: 2,
     url:
-      "https://media-exp1.licdn.com/dms/image/C4D03AQExheo0sff_yQ/profile-displayphoto-shrink_200_200/0/1590072898568?e=1630540800&v=beta&t=_W-gWZewSBYQ-UAjpGvR8h_8Vvo202IHQQissbK2aKc",
+      // "https://media-exp1.licdn.com/dms/image/C4D03AQExheo0sff_yQ/profile-displayphoto-shrink_200_200/0/1590072898568?e=1630540800&v=beta&t=_W-gWZewSBYQ-UAjpGvR8h_8Vvo202IHQQissbK2aKc",
+      "https://rbobkoskie1.github.io/react-att/images/Family_2.jpg",
   },
   {
     id: 3,
     url:
-      "https://yt3.ggpht.com/pe57RF1GZibOWeZ9GwRWbjnLDCK2EEAeQ3u4iMAFNeaz-PN9uSsg1p2p32TZUedNnrUhKfoOuMM=s900-c-k-c0x00ffffff-no-rj",
+      //"https://yt3.ggpht.com/pe57RF1GZibOWeZ9GwRWbjnLDCK2EEAeQ3u4iMAFNeaz-PN9uSsg1p2p32TZUedNnrUhKfoOuMM=s900-c-k-c0x00ffffff-no-rj",
+      "https://rbobkoskie1.github.io/react-att/images/ROB_BADGE_PIC.jpg",
   },
 ];
 
