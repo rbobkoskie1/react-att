49e6dcab6619e498ba6cc613d71b8a3010eef1f1
BOBKOSKIE <rb868x@att.com>
2022-05-30 18:33:17 -0400

    Updates

--- precache-manifest.9cc21a114afdb3410b94ca6448d2b089.js
+++ precache-manifest.9cc21a114afdb3410b94ca6448d2b089.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "15a77f5a79de1532e7eb",
-    "url": "/react-att/static/css/main.f5de3324.chunk.css"
+    "revision": "3a38abfbfa3733344c60",
+    "url": "/react-att/static/css/main.23acd1a1.chunk.css"
   },
   {
-    "revision": "15a77f5a79de1532e7eb",
-    "url": "/react-att/static/js/main.15a77f5a.chunk.js"
+    "revision": "3a38abfbfa3733344c60",
+    "url": "/react-att/static/js/main.3a38abfb.chunk.js"
   },
   {
     "revision": "0b5adef5a8e99730695f",
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/react-att/static/js/runtime~main.fe571902.js"
   },
   {
-    "revision": "a7e553c6eac628f2c4e0a54c04a644e0",
+    "revision": "63eba12ea1d930b7db5b9b975afe2d0c",
     "url": "/react-att/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-att/precache-manifest.1c293d1a8295f14f025a348d7328198f.js"
+  "/react-att/precache-manifest.9cc21a114afdb3410b94ca6448d2b089.js"
 );
 
 workbox.clientsClaim();
