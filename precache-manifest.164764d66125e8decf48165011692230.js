3009a0c61031646b30090770b7bbec21ad04628b
BOBKOSKIE <rb868x@att.com>
2022-05-30 11:59:28 -0400

    Updates

---
+++ precache-manifest.164764d66125e8decf48165011692230.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "a9559f7c6b9e61b05ecd",
+    "url": "/react-att/static/css/main.edb6831c.chunk.css"
+  },
+  {
+    "revision": "a9559f7c6b9e61b05ecd",
+    "url": "/react-att/static/js/main.a9559f7c.chunk.js"
+  },
+  {
+    "revision": "4bb05d8fed1e4d6e8559",
+    "url": "/react-att/static/js/1.4bb05d8f.chunk.js"
+  },
+  {
+    "revision": "2e7b5cb7ef8593ac2e7c",
+    "url": "/react-att/static/js/2.2e7b5cb7.chunk.js"
+  },
+  {
+    "revision": "11e82479ee1d9b5f92bf",
+    "url": "/react-att/static/js/runtime~main.11e82479.js"
+  },
+  {
+    "revision": "14343af8a760b7f93486101cfbae406a",
+    "url": "/react-att/static/media/Family_1.14343af8.jpg"
+  },
+  {
+    "revision": "977fceb4a4db2573e00243953f2bb269",
+    "url": "/react-att/static/media/Family_2.977fceb4.jpg"
+  },
+  {
+    "revision": "52c0ab6ca40ae5aadc1f6aa3fec72da4",
+    "url": "/react-att/static/media/ROB_BADGE_PIC.52c0ab6c.jpg"
+  },
+  {
+    "revision": "2503a37b0737408c25e4dd720a57fe8d",
+    "url": "/react-att/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-att/precache-manifest.6d3b70e8491f0834a7e21626184a84bf.js"
+  "/react-att/precache-manifest.164764d66125e8decf48165011692230.js"
 );
 
 workbox.clientsClaim();
